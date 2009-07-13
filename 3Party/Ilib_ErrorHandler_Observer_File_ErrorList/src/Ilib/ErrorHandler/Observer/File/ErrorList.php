204992ee3feccdfc6d157a206fa4fe66ea2563dc
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2009-07-13 11:07:57 +0000

    Corrected parse bug and rss file

--- 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList.php
+++ 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList.php
@@ -16,15 +16,7 @@ class Ilib_ErrorHandler_Observer_File_ErrorList
     
     private function getRawErrorList()
     {
-        $errors = array();
-        $handle = fopen($this->error_file, "r");
-        while (!feof($handle)) {
-           $buffer = fgets($handle, 4096);
-           if (empty($buffer) OR !is_string($buffer)) continue;
-           // $errors[] = unserialize($buffer); if buffer is array.
-           $errors[] = $buffer;
-        }
-        fclose($handle);
+        $errors = file($this->error_file);
         return $errors;
     }
     
@@ -38,7 +30,7 @@ class Ilib_ErrorHandler_Observer_File_ErrorList
         return $this->getParsedErrorList(true);
     }
     
-    private function getParsedErrorList($unique = false)
+    private function getParsedErrorList($only_unique = false)
     {
         $errors = $this->getRawErrorList();
 
@@ -47,29 +39,34 @@ class Ilib_ErrorHandler_Observer_File_ErrorList
 
         if(!empty($errors)) {
             foreach ($errors AS $error_string) {
-                if(!ereg("^([a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) ([a-zA-Z0-9]+) \[([a-zA-Z0-9]+)\] ([a-zA-Z0-9]+): (.+) in ([][a-zA-Z0-9/\_.-]*) line ([][a-zA-Z0-9]*) \(Request: ([][a-zA-Z0-9/\._%+&?-\=]*)\)", $error_string, $params)) {
+                // /modules/debtor/list.php?text=&status=2&sorting=0&from_date=01-06-2009&to_date=31-06-2009&type=invoice&contact_id=&product_id=&search=Find
+                if(!ereg("^([a-zA-Z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) ([a-zA-Z0-9]+) \[([a-zA-Z0-9]+)\] ([a-zA-Z0-9]+): (.+) in ([][a-zA-Z0-9/\_.-]*) line ([][a-zA-Z0-9]*) \(Request: ([][a-zA-Z0-9/\._%+&?\=-]*)\)", $error_string, $params)) {
                     $error['message'] = 'Unable to parse error line!';
+                    $error['date'] = date(DATE_RFC822);
+                    $error['type'] = 'Internal';
+                    $error['file'] = '[not given]';
+                    $error['line'] = '[not given]';
+                    $error['request'] = '[not given]';
+                } else {
+                    $error['date'] = (isset($params[1])) ? date(DATE_RFC822, strtotime($params[1])) : date(DATE_RFC822);
+
+                    $error['type'] = '';
+                    // We choose not to use PEAR log identifier [2] and PEAR error type [3] as they are the same so fare
+                    // if(isset($params[2])) $error['type'] .= $params[2].' ';
+                    // if(isset($params[3])) $error['type'] .= '['.$params[3].'] ';
+                    if(isset($params[4])) $error['type'] .= $params[4];
+    
+                    $error['message'] = (isset($params[5])) ? $params[5] : '[no message]';
+                    $error['file'] = (isset($params[6])) ? $params[6] : '[not given]';
+                    $error['line'] = (isset($params[7])) ? $params[7] : '[not given]';
+                    $error['request'] = (isset($params[8])) ? $params[8] : '[not given]';
                 }
-                $error['date'] = (isset($params[1])) ? date(DATE_RFC822, strtotime($params[1])) : date(DATE_RFC822);
-
-                $error['type'] = '';
-                // We choose not to use PEAR log identifier [2] and PEAR error type [3] as they are the same so fare
-                // if(isset($params[2])) $error['type'] .= $params[2].' ';
-                // if(isset($params[3])) $error['type'] .= '['.$params[3].'] ';
-                if(isset($params[4])) $error['type'] .= $params[4];
-
-                $error['message'] = (isset($params[5])) ? $params[5] : '[no message]';
-                $error['file'] = (isset($params[6])) ? $params[6] : '[not given]';
-                $error['line'] = (isset($params[7])) ? $params[7] : '[not given]';
-                $error['request'] = (isset($params[8])) ? $params[8] : '[not given]';
-
-                // $input['type'].": ".$input['message']." in ".$input['file']." line ".$input['line']. " (Request: ".$_SERVER['REQUEST_URI'].")";
-
-
-                if($unique && in_array(md5($error['type'].$error['message'].$error['file'].$error['line']), $unique)) {
+                
+                $unique_key = md5($error['type'].$error['message'].$error['file'].$error['line']);
+                if($only_unique && in_array($unique_key, $unique)) {
                     continue;
                 }
-                $unique[] = md5($error['type'].$error['message'].$error['file'].$error['line']);
+                $unique[] = $unique_key;
 
                 if($error['file'] == '') {
                     $error['file'] = 'URL: '.$error['request'];
