aa47b2dbae2187dd0b475cdd979df4a07c1e5834
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2010-01-17 21:17:41 +0000

    Example as to how we can start handling errors?

--- 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList/Controller/Index.php
+++ 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList/Controller/Index.php
@@ -1,16 +1,61 @@
 <?php
 
-class Ilib_ErrorHandler_Observer_File_ErrorList_Controller_Index extends k_Controller
+class Ilib_ErrorHandler_Observer_File_ErrorList_Controller_Index extends k_Component
 {
-    public $map = array(
-        'rss' => 'Ilib_ErrorHandler_Observer_File_ErrorList_Controller_View_RSS',
-        'all' => 'Ilib_ErrorHandler_Observer_File_ErrorList_Controller_View_All',
-        'unique' => 'Ilib_ErrorHandler_Observer_File_ErrorList_Controller_View_Unique'
-    );
+    protected $errorlist;
+    protected $translation;
+    protected $template;
 
-    
-    public function GET()
+    function __construct(Ilib_ErrorHandler_Observer_File_ErrorList $list, Translation2_Admin $translation, k_TemplateFactory $template)
     {
-        return 'Intentionally left blank';
+        $this->errorlist = $list;
+        $this->translation = $translation;
+        $this->template = $template;
+    }
+
+    function renderHtml()
+    {
+        if ($this->query('unique')) {
+            $data['items'] = $this->errorlist->getUnique();
+        } else {
+            $data['items'] = $this->errorlist->getAll();
+        }
+
+        try {
+            $translation = $this->translation;
+            $data['has_translation'] = true;
+        }
+        catch(ReflectionException $e) {
+            $data['has_translation'] = false;
+        }
+
+        $tpl = $this->template->create('Ilib/ErrorHandler/Observer/File/ErrorList/templates/errorlist');
+        return $tpl->render($this, $data);
+    }
+
+    function renderXml()
+    {
+        if ($this->query('unique')) {
+            $data['items'] = $this->errorlist->getUnique();
+        } else {
+            $data['items'] = $this->errorlist->getAll();
+        }
+
+        $tpl = $this->template->create('Ilib/ErrorHandler/Observer/File/ErrorList/templates/errorlist-rss');
+        $output = $tpl->render($this, $data);
+
+        $response = new k_HttpResponse(200, $output);
+        $response->setContentType("Content-Type: application/rss+xml");
+
+        return $response;
+    }
+
+    public function postForm()
+    {
+        if ($this->body('deletelog')) {
+            $this->errorlist->delete();
+        }
+
+        return new k_SeeOther($this->url());
     }
 }
--- 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList/templates/errorlist.tpl.php
+++ 3Party/Ilib_ErrorHandler_Observer_File_ErrorList/src/Ilib/ErrorHandler/Observer/File/ErrorList/templates/errorlist.tpl.php
@@ -1,12 +1,17 @@
 <h1>Errorlog</h1>
 
+<ul>
+	<li><a href="<?php e(url(null, array('unique' => true))); ?>">Unique</a></li>
+	<li><a href="<?php e(url(null . '.xml')); ?>">Rss</a></li>
+
+</ul>
+
 <p><form method="post" action="<?php e(url('.')); ?>"><strong>When you have corrected errors, you have to delete the log.</strong> <input type="submit" name="deletelog" value="Delete now" /></form></p>
 
-
-<?php if(isset($items) && is_array($items)): ?> 
+<?php if(isset($items) && is_array($items)): ?>
     <?php foreach($items AS $item): ?>
         <p>
-            <strong><?php e($item['title']); ?></strong> 
+            <strong><?php e($item['title']); ?></strong>
             <?php e($item['description']); ?><br />
             <?php e($item['pubDate']); ?>: <em><a href="<?php e($item['link']); ?>"><?php e($item['link']); ?></a></em>
         <?php if($has_translation && ereg("^Translation2: Missing translation for \"([a-zA-ZæøåÆØÅ _-]+)\" on pageID: \"([a-zA-ZæøåÆØÅ _-]+)\"", $item['title'], $params)): ?>
